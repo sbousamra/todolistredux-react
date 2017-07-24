998465924beade5b2809107333704a7873f1f5ed
Sebastian Bou-Samra <sebastian.bousamra@hotmail.com>
2017-07-24 14:56:14 +1000

    got rid of react scripts

--- server/app.js
+++ server/app.js
@@ -6,11 +6,11 @@ const lodash = require('lodash');
 
 const app = express();
 app.use(cookieParser());
-app.use(express.static(path.resolve(__dirname, '..', 'build')));
+app.use(express.static(path.resolve(__dirname, '..', 'public')));
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html')); 
+  res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html')); 
 });
 
 var PORT = process.env.PORT || 9000;
