b529d3a3ceb8a75e651444cebad00c25487e2ebe
Sebastian Bou-Samra <sebastian.bousamra@hotmail.com>
2017-08-11 17:01:18 +1000

    changed browserHistory module to createBrowserHistory since not working with latest version 4.0.1

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createBrowserHistory} from 'history';
 import {applyMiddleware, createStore} from 'redux';
 import logger from 'redux-logger'
 import {Provider} from 'react-redux';
-import {BrowserRouter as Router, Route, Redirect, browserHistory} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 import {syncHistoryWithStore} from 'react-router-redux';
 import rootReducer from './reducers/rootReducer';
 import Home from './containers/home';
 import Signup from './containers/signup';
 
 const store = createStore(rootReducer, applyMiddleware(logger))
+const history = syncHistoryWithStore(createBrowserHistory(), store)
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
+    <Router history={history}>
       <div>
         <Route exact path="/" component={() => <Home/>}/>
         <Route path="/signup" component={() => <Signup/>}/>
