b8b017a6d1fa3d87ac4b470230d0187e4a0c7484
Sebastian Bou-Samra <sebastian.bousamra@hotmail.com>
2017-08-17 17:30:16 +1000

    added back lodash.extend on signup to test state change

--- src/reducers/account.js
+++ src/reducers/account.js
@@ -4,7 +4,7 @@ import lodash from 'lodash';
 function account(state = {}, action) {
   switch (action.type) {
     case "SIGNUP":
-      return state
+      return lodash.extend({}, state, {signedUp: true})
     case "LOGIN":
       return lodash.extend({}, state, {twitterData: action.twitterData, loggedIn: true, username: action.username})
     case "LOGOUT":
